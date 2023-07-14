3dbd4a69cf45b54904b5c3eb47408d049a249e21
Quincy Bulla <37887069+dominionworkz@users.noreply.github.com>
2023-07-14 19:52:09 -0400

    added some updates to these files

--- finalChallenge/main.js
+++ finalChallenge/main.js
@@ -1,17 +1,3 @@
-/*const navButton = document.querySelector('.nav-toggle');
-const nav = document.querySelector('.header--nav');
-
-
-navButton.addEventListener('click', () => {
-    nav.classList.toggle('header--nav');
-    const navButton = document.querySelector('.nav-toggle');
-    const nav = document.querySelector('.header--nav');
-    
-    navButton.addEventListener('click', () => {
-        nav.classList.toggle('header--nav');
-    });
-})*/
-
 
 const navButton = document.querySelector('.nav-toggle');
 const nav = document.querySelector('.header--nav');
